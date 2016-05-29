e5c8970d5e593858fcc648fa7fd0c74fbba2e2e2
AlexFeudor <alex.feudor@gmail.com>
2016-05-29 12:13:33 +0300

    Создана функция Draw

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,6 +10,25 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int x1 = 1;
+            int y1 = 5;
+            char sym1 = '*';
+
+            Draw(x1, y1, sym1);
+
+            int x2 = 5;
+            int y2 = 10;
+            char sym2 = '#';
+
+            Draw(x2, y2, sym2);
+
+            Console.ReadLine();
+
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
